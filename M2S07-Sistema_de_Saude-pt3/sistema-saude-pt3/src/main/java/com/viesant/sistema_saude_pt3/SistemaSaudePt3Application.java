027056f122ce7a6d661c63aa8bb1228ed954ea53
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-07-17 11:44:37 +0200

    Start spring project

---
+++ M2S07-Sistema_de_Saude-pt3/sistema-saude-pt3/src/main/java/com/viesant/sistema_saude_pt3/SistemaSaudePt3Application.java
@@ -0,0 +1,13 @@
+package com.viesant.sistema_saude_pt3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSaudePt3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaSaudePt3Application.class, args);
+	}
+
+}
