5214feb1689effa36a8e2c75ab515a21194ad5d2
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-08-15 22:12:29 +0200

    [M2S10] Ex 01 - Mini Projeto - Cadastro de Médicos Spring (início)

---
+++ M2S10-Cadastro_de_Medicos/CadastroMedicos/src/test/java/com/viesant/CadastroMedicos/CadastroMedicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viesant.CadastroMedicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroMedicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
