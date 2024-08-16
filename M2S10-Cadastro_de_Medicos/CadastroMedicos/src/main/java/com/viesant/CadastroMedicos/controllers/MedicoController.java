82aac27345159782c683ccf6b6606529a4ba2b14
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-08-16 13:55:02 +0200

    [M2S10] Ex 05 - Mini Projeto - Cadastro de Médicos Spring (listagem)

--- M2S10-Cadastro_de_Medicos/CadastroMedicos/src/main/java/com/viesant/CadastroMedicos/controllers/MedicoController.java
+++ M2S10-Cadastro_de_Medicos/CadastroMedicos/src/main/java/com/viesant/CadastroMedicos/controllers/MedicoController.java
@@ -1,30 +1,55 @@
 package com.viesant.CadastroMedicos.controllers;
 
+import com.viesant.CadastroMedicos.DTO.MedicoFilterResponse;
 import com.viesant.CadastroMedicos.DTO.MedicoRequest;
+import com.viesant.CadastroMedicos.enums.EspecialidadesEnum;
 import com.viesant.CadastroMedicos.models.Medico;
 import com.viesant.CadastroMedicos.services.MedicoService;
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
-  @Autowired
-  private MedicoService medicoService;
+  @Autowired private MedicoService medicoService;
 
   @PostMapping
-  public Medico createMedico(@RequestBody MedicoRequest medicoRequest){
+  public Medico createMedico(@RequestBody MedicoRequest medicoRequest) {
     return medicoService.createMedico(medicoRequest);
   }
 
   @PutMapping("{id}")
-  public Medico updateMedico(@PathVariable long id, @RequestBody MedicoRequest medicoRequest){
+  public Medico updateMedico(@PathVariable long id, @RequestBody MedicoRequest medicoRequest) {
     return medicoService.updateMedico(id, medicoRequest);
   }
 
+  @GetMapping
+  public Page<MedicoFilterResponse> listarMedicos(
+          @RequestParam(required = false) String nome,
+          @RequestParam(required = false) EspecialidadesEnum especialidade,
+          @RequestParam(required = false) LocalDate dataNascimento,
+//          @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataNascimento,
+          @PageableDefault(size = 10, sort = "nome") Pageable pageable) {
+
+    return medicoService.listarMedicos(nome, especialidade, dataNascimento, pageable);
+  }
+
+  // para testes
+  @GetMapping("/all")
+  public List<Medico> getAllMedicos() {
+    return medicoService.getAllMedicos();
+  }
 }
--- M2S10-Cadastro_de_Medicos/CadastroMedicos/src/main/java/com/viesant/CadastroMedicos/services/MedicoService.java
+++ M2S10-Cadastro_de_Medicos/CadastroMedicos/src/main/java/com/viesant/CadastroMedicos/services/MedicoService.java
@@ -2,11 +2,20 @@ package com.viesant.CadastroMedicos.services;
 
 import static com.viesant.CadastroMedicos.mappers.MedicoMapper.toEntity;
 
+import com.viesant.CadastroMedicos.DTO.MedicoFilterResponse;
 import com.viesant.CadastroMedicos.DTO.MedicoRequest;
+import com.viesant.CadastroMedicos.enums.EspecialidadesEnum;
 import com.viesant.CadastroMedicos.mappers.MedicoMapper;
 import com.viesant.CadastroMedicos.models.Medico;
 import com.viesant.CadastroMedicos.repositories.MedicoRepository;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -29,10 +38,30 @@ public class MedicoService {
             .orElseThrow(() -> new RuntimeException("Médico não encontrado com ID: " + id));
 
     if (medicoRepository.existsByCrmAndIdNot(medicoRequest.crm(), id)) {
-      throw new IllegalArgumentException("CRM já cadastrado por outro médico: " + medicoRequest.crm());
+      throw new IllegalArgumentException(
+          "CRM já cadastrado por outro médico: " + medicoRequest.crm());
     }
 
     MedicoMapper.updateEntity(medicoRequest, medico);
     return medicoRepository.save(medico);
+  }
+
+  public List<Medico> getAllMedicos() {
+    return medicoRepository.findAll();
+  }
+
+  public Page<MedicoFilterResponse> listarMedicos(
+      String nome,
+      EspecialidadesEnum especialidade,
+      LocalDate dataNascimento,
+      Pageable pageable) {
+
+    Page<Medico> medicosPage =
+        medicoRepository.findByFilters(nome, especialidade, dataNascimento, pageable);
+
+    return medicosPage.map(
+        medico ->
+            new MedicoFilterResponse(
+                medico.getNome(), medico.getEspecialidade(), medico.getDataNascimento()));
   }
 }
